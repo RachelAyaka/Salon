80be4dd6f108e87920178628e22ee1a1f0523931
Rachel Lin <linayaka117@gmail.com>
2025-04-06 12:23:45 -0700

    changed teh backend vercel http url

--- salon-app/src/utils/constants.js
+++ salon-app/src/utils/constants.js
@@ -1,2 +1,2 @@
 export const BASE_URL = 'http://localhost:8000'
-export const API_URL = import.meta.env.VITE_API_URL || 'https://salon-backend-fgtk3bgrb-rachelayakas-projects.vercel.app/'
+export const API_URL = import.meta.env.VITE_API_URL || 'https://salon-backend-two.vercel.app/'
